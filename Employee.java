b508194521eba9dc42a224eb0be29a017dd9a754
Prachet Patel <prachet.edu@gamil.com>
2023-02-21 19:01:16 +0530

    Modified Employee

--- Employee.java
+++ Employee.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Employee{
-    float dearnessallowance,TDS;
-    int salary;
+    float dearnessallowance;
+    int basicsalary;
     String Name;
     static int count=1000;
     public static void main(String[] args) {
@@ -12,7 +12,7 @@ class Employee{
             System.out.println("1.Enter Details");
             System.out.println("2.Calculate and Display Details");
             System.out.println("3.Exit");
-            System.out.println("Choose a number:");
+            System.out.print("Choose a number:");
             n=x.nextInt();
             switch(n){
                 case 1:
@@ -21,12 +21,11 @@ class Employee{
                    i++;
                 break;
                 case 2:
-                System.out.println("Enter your Reference ID Number:");
+                System.out.print("Enter your Reference ID Number:");
                 j=x.nextInt();
                 j-=1000;
-                if(j<i&&j>=0){ 
+                if(j<i&&j>=0)
                     obj[j].displaydetails();
-                }
                 else
                     System.out.println("Invalid Reference ID");
                 break;
@@ -41,27 +40,27 @@ class Employee{
     }
     void input(){
         Scanner in=new Scanner(System.in);
-        System.out.println("Enter Your Name : ");
+        System.out.print("Enter Your Name : ");
         this.Name=in.nextLine();
-        System.out.println("Enter Your Annual Salary : ");
-        this.salary=in.nextInt();
+        System.out.print("Enter Your Annual Salary : ");
+        this.basicsalary=in.nextInt();
+        System.out.println();
         System.out.println("Your input reference ID is : "+count);
         count++;
     }
     void displaydetails(){
-        System.out.println("Your Name is: "+this.Name);
-        System.out.println("Your Annual Salary : "+this.salary);
-        this.dearnessallowance=((float)0.74*(float)(this.salary));
-        float grossSalary=this.salary+this.dearnessallowance;
+        this.dearnessallowance=((float)0.74*(float)(this.basicsalary));
+        float grossSalary=this.basicsalary+this.dearnessallowance;
+        System.out.println("Your Name is                     : "+this.Name);
+        System.out.println("Your Annual Salary               : "+this.basicsalary);
         System.out.println("Calculated Dearness Allowance is : "+this.dearnessallowance);
-        this.TDS=(float)(0.10)*grossSalary;
         if(grossSalary>100000){
-            System.out.println("TDS charged is : "+this.TDS);
-            System.out.println("Your Net Salary is : "+grossSalary);
+        System.out.println("TDS charged is                   : "+((float)(0.10)*grossSalary));
+        System.out.println("Your Net Salary is               : "+grossSalary);
         }
         else{
-            System.out.println("TDS is : NIL");
-            System.out.println("Your Net Salary is : "+grossSalary);
+        System.out.println("TDS charged is                   : NIL");
+        System.out.println("Your Net Salary is               : "+grossSalary);
         }
     }
 } 
